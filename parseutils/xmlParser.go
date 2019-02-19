b56eebf374590b52dc8c62839adeba369d66b49b
Samy Metadjer <samy.metadjer@gmail.com>
2019-02-19 16:59:00 +0100

    feat(parseutils): methods to check if text contains cat and methods to extract based on tag

--- parseutils/xmlParser.go
+++ parseutils/xmlParser.go
@@ -37,69 +37,57 @@ func ParseXMLFile(input string, output string) error {
 		switch v := t.(type) {
 		case xml.StartElement:
 			if v.Name.Local == "page" {
-				isOnPage = true
-			}
-			if v.Name.Local == "title" {
-				isOnTitle = true
-			}
-			if v.Name.Local == "text" {
-				isOnContent = true
-			}
-		case xml.EndElement:
-			if v.Name.Local == "page" {
-				isOnPage = false
-			}
-			if v.Name.Local == "title" {
-				isOnTitle = false
-			}
-			if v.Name.Local == "text" {
-				isOnContent = false
-			}
-		case xml.CharData:
-			if isOnPage {
-				if isOnTitle {
-					//	fmt.Println("Titre : " + string(v))
-					outputFile.Write([]byte("<title>"))
-					outputFile.Write([]byte(v))
-					outputFile.Write([]byte("</title>\n"))
-				}
-				if isOnContent {
-					//	fmt.Println("Contenu : " + string(v))
-					outputFile.Write([]byte("<text>"))
-					outputFile.Write([]byte(v))
-					outputFile.Write([]byte("</text>\n"))
+				total++
+				title := extract("title", decoder)
+				content := extract("text", decoder)
+				if contains(content) {
+					outputFile.WriteString(title)
+					outputFile.WriteString(content)
+					pageProcessed++
 				}
 			}
 		}
 	}
+
+	fmt.Printf("Nombre de pages extraites: %v sur un total de %v\n", pageProcessed, total)
 	return nil
 }
 
-func decode(path string) {
-	xmlFile, err := os.Open(path)
-	if err != nil {
-		return
+func contains(text string) bool {
+	for _, v := range categories {
+		if strings.Contains(text, v) {
+			return true
+		}
 	}
-	decoder := xml.NewDecoder(xmlFile)
+	return false
+}
+
+func extract(tag string, decoder *xml.Decoder) string {
+	isOn := false
+	result := ""
 
 	for {
-		// Read tokens from the XML document in a stream.
-		t, _ := decoder.Token()
-		if t == nil {
+		t, err := decoder.Token()
+		if err != nil {
+			fmt.Printf("decoder.Token() failed with '%s'\n", err)
 			break
 		}
-		// Inspect the type of the token just read.
-		switch se := t.(type) {
+
+		switch v := t.(type) {
 		case xml.StartElement:
-			// If we just read a StartElement token
-			// ...and its name is "page"
-			if se.Name.Local == "page" {
-				var p Page
-				// decode a whole chunk of following XML into the
-				// variable p which is a Page (se above)
-				decoder.DecodeElement(&p, &se)
-				// Do some stuff with the page.
+			if v.Name.Local == tag {
+				isOn = true
+			}
+		case xml.EndElement:
+			if v.Name.Local == tag {
+				isOn = false
+				return result
+			}
+		case xml.CharData:
+			if isOn {
+				result = string(v[:])
 			}
 		}
 	}
+	return result
 }
