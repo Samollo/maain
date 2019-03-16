38832b441ff51b2ac185fffe8d3be8f5b69bb5c0
Samy Metadjer <samy.metadjer@gmail.com>
2019-03-16 18:22:08 +0100

    feat(parser): word struct implemented

---
+++ parseutils/word.go
@@ -0,0 +1,27 @@
+package parseutils
+
+type Pair struct {
+	id  int
+	val int
+}
+
+type Word struct {
+	value string
+	freq  int
+}
+
+func NewWord(value string) *Word {
+	return &Word{value: value, freq: 1}
+}
+
+func (w *Word) Increment() {
+	w.freq += 1
+}
+
+func (w *Word) Frequence() int {
+	return w.freq
+}
+
+func (w *Word) String() string {
+	return w.value
+}
