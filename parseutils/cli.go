7c04fdf6416b0e8ce8e8f6126f742340ea5bf972
Mohammed Ansari <mohammed.ansari@hotmail.fr>
2019-03-18 15:56:39 +0100

    Update cli.go

--- parseutils/cli.go
+++ parseutils/cli.go
@@ -1,35 +1,43 @@
 package parseutils
 
-import "github.com/Samollo/maain/constants"
-
 type CLI struct {
 	c []float32
 	l []int
 	i []int
 }
 
+func (cli *CLI) GetC() []float32 {
+	return cli.c
+}
+
+func (cli *CLI) GetL() []int {
+	return cli.l
+}
+func (cli *CLI) GetI() []int {
+	return cli.i
+}
+
 func NewCLI() *CLI {
 	return &CLI{
 		c: make([]float32, 0),
-		l: make([]int, constants.PagesToExtract),
+		l: make([]int, 0),
 		i: make([]int, 0),
 	}
 }
 
-func (cli *CLI) AddPage(pageId int, links Links) error {
-	l := links
+func (cli *CLI) AddPage(pageId int, links []int) error {
+	coef := float32(1) / float32(len(links))
 
-	coef := float32(1) / float32(len(l))
-
-	for _, value := range l {
+	for _, value := range links {
 		cli.c = append(cli.c, coef)
 		cli.i = append(cli.i, value)
 	}
 
 	nbLink := 0
-	if len(cli.l) > 1 {
-		nbLink = cli.l[len(cli.l)-1] + 1
+	if len(cli.l) == 0 {
+		cli.l = append(cli.l, nbLink)
 	}
+	nbLink = cli.l[len(cli.l)-1] + len(links)
 
 	cli.l = append(cli.l, nbLink)
 	return nil
