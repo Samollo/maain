c2c77ea71ec3799c93a35144d1bafeeb3eabcfc3
Samy Metadjer <samy.metadjer@gmail.com>
2019-03-19 18:10:42 +0100

    feat(wordpages): wordPagesRelation implemented with basic getters

--- parseutils/relations.go
+++ parseutils/relations.go
@@ -29,6 +29,22 @@ func NewWordPagesRelation(words []string, pagesName ...string) *WordsPagesRelati
 	}
 }
 
+func (wpr *WordsPagesRelation) Words() []string {
+	return wpr.words
+}
+
+func (wpr *WordsPagesRelation) Pages() []string {
+	return wpr.pages
+}
+
+func (wpr *WordsPagesRelation) WordsID() map[string]int {
+	return wpr.wordsID
+}
+
+func (wpr *WordsPagesRelation) PagesID() map[string]int {
+	return wpr.pagesID
+}
+
 func (wpr *WordsPagesRelation) WordByID(id int) string {
 	return wpr.words[id]
 }
@@ -45,7 +61,7 @@ func (wpr *WordsPagesRelation) PageByValue(title string) int {
 	return wpr.pagesID[title]
 }
 
-func (wpr *WordsPagesRelation) addPage(title string, corpus string) {
+func (wpr *WordsPagesRelation) AddPage(title string, corpus string, stopWords map[string]int) {
 	//if page not already processed, then add it
 	if _, ok := wpr.pagesID[title]; !ok {
 		wpr.pages = append(wpr.pages, title)
@@ -53,11 +69,11 @@ func (wpr *WordsPagesRelation) addPage(title string, corpus string) {
 	}
 
 	//update the relations
-	words, total := extractWords(title, corpus, nil, nil)
+	words, content := extractWords(title, corpus, nil, nil, stopWords)
 	for _, w := range words {
 		//if word from page is into our hashmap then update the relations
 		if index, ok := wpr.wordsID[w.String()]; !ok {
-			page := &Pair{wpr.pagesID[title], w.Frequence() / total}
+			page := &Pair{wpr.pagesID[title], w.Frequence() / len(content)}
 			wpr.relations[index] = append(wpr.relations[index], page)
 			wpr.pages = append(wpr.pages, title)
 			wpr.pagesID[title] = len(wpr.pages) - 1
