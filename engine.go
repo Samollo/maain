3ffd9660d0ae391b42b1cc0133377d3dd5f72568
Mohammed Ansari <mohammed.ansari@hotmail.fr>
2019-03-20 16:30:02 +0100

    Adding exemple

--- engine.go
+++ engine.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/Samollo/maain/crawler"
+	"github.com/Samollo/maain/parseutils"
 )
 
 func main() {
 	fmt.Println("WEB Search Engine by Ansari & Metadjer")
-	args := os.Args[1:]
-	c := crawler.NewCrawler(args[0])
-	c.Prepare()
-	c.CLI.PageRank()
+	/*	args := os.Args[1:]
+		c := crawler.NewCrawler(args[0])
+		c.Prepare()
+		c.CLI.PageRank()
+	*/
+	cli := parseutils.NewCLI()
+	cli.AddPage([]int{1, 3})
+	cli.AddPage([]int{3})
+	cli.AddPage([]int{4})
+	cli.AddPage([]int{2, 4})
+	cli.AddPage([]int{})
+
+	cli.PageRank()
+
 }
