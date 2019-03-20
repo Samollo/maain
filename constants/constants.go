e3900298a153d906d2cd4cbfcc249bad443f2be0
Samy Metadjer <samy.metadjer@gmail.com>
2019-03-20 17:05:11 +0100

    refactor(const): add constants about PageRank and CLI

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,10 @@
 package constants
 
-const PagesToExtract = 5
+const PagesToExtract = 200
 const Output = "dataset.xml"
 const Stopwords = "stopwords-fr.txt"
 const WordsToKeep = 10000
+
 const DumpFactor = 0.85
+const Epsilon = 0.0001
+const Zap = true
