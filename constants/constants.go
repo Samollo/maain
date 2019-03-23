86ec241337ccf518685b71ba3c0127f8bb3b7b81
Samy Metadjer <samy.metadjer@gmail.com>
2019-03-23 15:21:19 +0100

    feat: new constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,11 +1,15 @@
 package constants
 
 const PagesToExtract = 200000
+const TakeAll = true
+const Input = "complete.xml"
 const Output = "dataset.xml"
 const Stopwords = "stopwords-fr.txt"
-const WordsToKeep = 10000
+const ResultFile = "wordpages"
+const WordsToKeep = 900000
 
-const PageRankBoost = 10
+const PageRankBoost = 1000
 const DumpFactor = 0.85
 const Epsilon = 0.0001
 const Zap = true
+const RegexWord = "[a-z]{3,}"
